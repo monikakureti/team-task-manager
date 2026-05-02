f539e7b34bec8e4fee40ef9725a8d40049a8eeff
Kureti Monika <monikakureti005@gmail.com>
2026-05-02 20:52:10 +0530

    update backend url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const AUTH_API = "http://localhost:5000/api/auth";
+const AUTH_API =
+  "https://team-task-manager-production-f53f.up.railway.app/api/auth";
 
-const TASK_API = "http://localhost:5000/api/tasks";
+const TASK_API =
+  "https://team-task-manager-production-f53f.up.railway.app/api/tasks";
 
 export const registerUser = async (userData) => {
   const response = await axios.post(
@@ -22,17 +24,29 @@ export const loginUser = async (userData) => {
   return response.data;
 };
 
-export const getTasks = async () => {
-  const response = await axios.get(TASK_API);
+export const createTask = async (
+  taskData,
+  token
+) => {
+  const response = await axios.post(
+    TASK_API,
+    taskData,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
   return response.data;
 };
 
-export const createTask = async (taskData) => {
-  const response = await axios.post(
-    TASK_API,
-    taskData
-  );
+export const getTasks = async (token) => {
+  const response = await axios.get(TASK_API, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
   return response.data;
 };
